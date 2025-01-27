1d083f118527bef901701427ed69f3adb753bc31
Daria Marukhlenko <da.marukhlenko@gmail.com>
2025-01-28 01:20:46 +1100

    Add new url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  site: 'https://dariamrk.github.io',
-  base: '/maru',
-  integrations: [tailwind(), react()],
+  site: 'https://maru.lc',
+  integrations: [tailwind()],
 });
